6af62905362b631f5b6383cb9f1c8ee324991fcb
Zakaria Hassan <zakariahassan00@hotmail.com>
2020-01-26 20:16:32 +0200

    add sidemenu routing

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import Button from "./../button/Button";
 import "./sideMenu.scss";
+
+const RenderListItem = ({ name, icon, to = "/" }) => {
+  return (
+    <Link to={`/main/${to}`}>
+      <li className="sidemenu__list-item">
+        <span>
+          <FontAwesomeIcon className="sidemenu__icon" icon={icon} />
+        </span>
+        {name}
+      </li>
+    </Link>
+  );
+};
 
 const SideMenu = () => {
   return (
@@ -9,33 +23,15 @@ const SideMenu = () => {
       <div className="sidemenu__brand">
         <h1>Contact</h1>
       </div>
+
       <div className="sidemenu__elements">
         <ul className="sidemenu__list">
-          <li className="sidemenu__list-item">
-            <span>
-              <FontAwesomeIcon className="sidemenu__icon" icon="user" />
-            </span>
-            Profile
-          </li>
-          <li className="sidemenu__list-item">
-            <span>
-              <FontAwesomeIcon className="sidemenu__icon" icon="envelope" />
-            </span>
-            Messages
-          </li>
-          <li className="sidemenu__list-item">
-            <span>
-              <FontAwesomeIcon className="sidemenu__icon" icon="bullhorn" />
-            </span>
-            News
-          </li>
-          <li className="sidemenu__list-item">
-            <span>
-              <FontAwesomeIcon className="sidemenu__icon" icon="video" />
-            </span>
-            Channels
-          </li>
+          <RenderListItem name="Profile" icon="user" to="profile" />
+          <RenderListItem name="Messages" icon="envelope" to="messages" />
+          <RenderListItem name="News" icon="bullhorn" to="news" />
+          <RenderListItem name="Channels" icon="video" to="channels" />
         </ul>
+
         <div className="sidemenu__channel">
           <p>Create Channel To Enjoy Group Chatting with Your Friends!</p>
           <Button color="success" type="rounded">
