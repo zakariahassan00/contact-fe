6b2d192f9269a60511101bf606830c944fea8ed0
Zakaria Hassan <zakariahassan00@hotmail.com>
2020-01-27 19:19:24 +0200

    add search functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -39,3 +39,26 @@ export const register = credentials => async dispatch => {
     });
   }
 };
+
+export const GET_FRIENDS = "get_friends";
+export const searchFriends = query => async dispatch => {
+  try {
+    const res = await axios.get(`${ENDPOINT}/api/friends?query=${query}`);
+
+    dispatch({ action: GET_FRIENDS, payload: res.data });
+  } catch (e) {
+    dispatch({ action: GET_FRIENDS, payload: null });
+  }
+};
+
+// search for all people including friends.
+export const GET_PEOPLE = "get_people";
+export const searchPeople = query => async dispatch => {
+  try {
+    const res = await axios.get(`${ENDPOINT}/api/people?query=${query}`);
+
+    dispatch({ type: GET_PEOPLE, payload: res.data });
+  } catch (e) {
+    dispatch({ type: GET_PEOPLE, payload: null });
+  }
+};
