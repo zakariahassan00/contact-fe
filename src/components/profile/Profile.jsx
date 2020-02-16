320b2ebce4f5dadc637287a0f1097f9ab61d1251
Zakaria Hassan <zakariahassan00@hotmail.com>
2020-02-16 16:07:46 +0200

    change the profile page style

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -7,28 +7,23 @@ import "./profile.scss";
 const Profile = props => {
   return (
     <div className="profile">
-      <div className="profile__header">
-        <div className="profile__cover"></div>
-      </div>
       <div className="profile__main">
-        <div className="profile__main--inner">
-          <div className="profile__avatar">
-            <img src={user.img} alt="profile" />
-          </div>
-          <div className="profile__name">
-            <h1>{user.name}</h1>
-          </div>
-          <div className="profile__stats">
-            Friends :{" "}
-            <span className="profile__stats--friends">{user.friends}</span>
-            Followers :{" "}
-            <span className="profile__stats--followers">{user.followers}</span>
-          </div>
-          <div className="profile__summary">{user.summary}</div>
-          <div className="profile__connect">
-            <Button type="rounded">Connect</Button>
-            <Button type="rounded">Follow</Button>
-          </div>
+        <div className="profile__avatar">
+          <img src={user.img} alt="profile" />
+        </div>
+        <div className="profile__name">
+          <h1>{user.name}</h1>
+        </div>
+        <div className="profile__stats">
+          Friends :{" "}
+          <span className="profile__stats--friends">{user.friends}</span>
+          Followers :{" "}
+          <span className="profile__stats--followers">{user.followers}</span>
+        </div>
+        <div className="profile__summary">{user.summary}</div>
+        <div className="profile__connect">
+          <Button type="rounded">Connect</Button>
+          <Button type="rounded">Follow</Button>
         </div>
       </div>
     </div>
